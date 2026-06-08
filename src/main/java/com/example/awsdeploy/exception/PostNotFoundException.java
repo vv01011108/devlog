13285108cd3c366ce1ac356093315f978f4e84e9
vv01011108 <vv01011108@gmail.com>
2026-06-08 15:35:50 +0900

    자동 배포 확인

--- src/main/java/com/example/awsdeploy/exception/PostNotFoundException.java
+++ src/main/java/com/example/awsdeploy/exception/PostNotFoundException.java
@@ -3,6 +3,6 @@ package com.example.awsdeploy.exception;
 public class PostNotFoundException extends RuntimeException {
 
     public PostNotFoundException(Long id) {
-        super("해당 글이 존재하지 않습니다. id = " + id);
+        super("해당 글이 존재하지 않습니다. id= " + id);
     }
 }
